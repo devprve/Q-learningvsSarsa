5155251e6c2e6c380c5ca015915290dd17a35559
devprve <106182202+devprve@users.noreply.github.com>
2023-10-02 22:51:59 -0700

    Update sarsa.py

--- sarsa.py
+++ sarsa.py
@@ -1,10 +1,21 @@
+!pip install gym==0.15.3
+!apt-get -y install python3-opengl
+!pip install pyopengl
+!pip install pyglet<=1.5.0
+!apt-get install swig
+
+import gym
+
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(400, 300))
+display.start()
+from gym.wrappers.monitoring.video_recorder import VideoRecorder
+
 import gym
 import numpy as np
 
-
-#MAX_NUM_EPISODES = 5000
-MAX_NUM_EPISODES = 5000
-STEPS_PER_EPISODE = 100 #  This is specific to MountainCar. May change with env
+MAX_NUM_EPISODES = 1000
+STEPS_PER_EPISODE = 200 #  This is specific to MountainCar. May change with env
 EPSILON_MIN = 0.005
 max_num_steps = MAX_NUM_EPISODES * STEPS_PER_EPISODE
 EPSILON_DECAY = 500 * EPSILON_MIN / max_num_steps
@@ -12,9 +23,7 @@ ALPHA = 0.05  # Learning rate
 GAMMA = 0.98  # Discount factor
 NUM_DISCRETE_BINS = 30  # Number of bins to Discretize each observation dim
 
-
-
-class SARSA_Learner(object):
+class Sarsa_Learner(object):
     def __init__(self, env):
         self.obs_shape = env.observation_space.shape
         self.obs_high = env.observation_space.high
@@ -24,14 +33,15 @@ class SARSA_Learner(object):
         self.action_shape = env.action_space.n
         # Create a multi-dimensional array (aka. Table) to represent the
         # Q-values
-        self.Q = np.zeros((self.obs_bins + 1, self.obs_bins + 1, self.action_shape))  # (51 x 51 x 3)
+        self.Q = np.zeros((self.obs_bins + 1, self.obs_bins + 1,
+                           self.action_shape))  # (51 x 51 x 3)
         self.alpha = ALPHA  # Learning rate
         self.gamma = GAMMA  # Discount factor
         self.epsilon = 1.0
-    
+
     def discretize(self, obs):
         return tuple(((obs - self.obs_low) / self.bin_width).astype(int))
-    
+
     def get_action(self, obs):
         discretized_obs = self.discretize(obs)
         # Epsilon-Greedy action selection
@@ -41,7 +51,8 @@ class SARSA_Learner(object):
             return np.argmax(self.Q[discretized_obs])
         else:  # Choose a random action
             return np.random.choice([a for a in range(self.action_shape)])
-    
+
+    #CHANGES HAVE BEEN MADE HERE TO CHANGE FROM Q-LEARNING TO SARSA
     def learn(self, obs, action, reward, next_obs, next_action):
         discretized_obs = self.discretize(obs)
         discretized_next_obs = self.discretize(next_obs)
@@ -49,35 +60,26 @@ class SARSA_Learner(object):
         td_error = td_target - self.Q[discretized_obs][action]
         self.Q[discretized_obs][action] += self.alpha * td_error
 
-
-
-
-
 def train(agent, env):
-    reward_list = list()
     best_reward = -float('inf')
     for episode in range(MAX_NUM_EPISODES):
         done = False
         obs = env.reset()
         total_reward = 0.0
+        action = agent.get_action(obs)
         while not done:
-            action = agent.get_action(obs)
             next_obs, reward, done, info = env.step(action)
             next_action = agent.get_action(next_obs)
-            agent.learn(obs, action, reward, next_obs,next_action)
+            agent.learn(obs, action, reward, next_obs, next_action)
             obs = next_obs
+            action = next_action
             total_reward += reward
         if total_reward > best_reward:
             best_reward = total_reward
         print("Episode#:{} reward:{} best_reward:{} eps:{}".format(episode,
-                                      total_reward, best_reward, agent.epsilon))
-        reward_listt.append(total_reward)
+                                     total_reward, best_reward, agent.epsilon))
     # Return the trained policy
     return np.argmax(agent.Q, axis=2)
-
-
-
-
 
 def test(agent, env, policy):
     done = False
@@ -90,16 +92,17 @@ def test(agent, env, policy):
         total_reward += reward
     return total_reward
 
+env = gym.make('MountainCar-v0')
+agent = Sarsa_Learner(env)
+learned_policy = train(agent, env)
 
+# Use the VideoRecorder to evalaute the agent and record video
+gym_monitor_path = "./gym_monitor_sarsa.mp4"
+video = VideoRecorder(env, gym_monitor_path)
 
-
-if __name__ == "__main__":
-    env = gym.make('MountainCar-v0')
-    agent = SARSA_Learner(env)
-    learned_policy = train(agent, env)
-    # Use the Gym Monitor wrapper to evalaute the agent and record video
-    gym_monitor_path = "./gym_monitor_output"
-    env = gym.wrappers.Monitor(env, gym_monitor_path, force=True)
-    for _ in range(1000):
-        test(agent, env, learned_policy)
-    env.close()
+for _ in range(500):
+    env.render()
+    video.capture_frame()
+    test(agent, env, learned_policy)
+video.close()
+env.close()
